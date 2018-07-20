ef5feab3ef4a66a03ccc89416303cfb6d5293a3a
Hanzei <hanzei@mailbox.org>
2018-07-20 14:45:36 +0200

    Refactor config.toml

--- assets/js/initMoment.js
+++ assets/js/initMoment.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    var time = moment().tz("{{ .Site.Params.timeZone }}").format("h:mm A");
+    var time = moment().tz("{{ .Site.Params.home.timeZone }}").format("h:mm A");
     $('#time').html(time);
 })
