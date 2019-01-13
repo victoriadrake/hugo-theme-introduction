819c0588650c23a85e6d1b10fc1a47c2db72a5ec
Maximilian Bosche <snow2k9@users.noreply.github.com>
2019-01-13 08:24:53 +0000

    Added parameter for timeformat (#124)

--- assets/js/initMoment.js
+++ assets/js/initMoment.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    var time = moment().tz("{{ .Site.Params.home.timeZone }}").format("h:mm A");
+    var time = moment().tz("{{ .Site.Params.home.timeZone }}").format("{{ .Site.Params.home.timeFormat }}");
     $("#time").html(time);
 })
