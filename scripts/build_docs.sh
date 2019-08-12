a17ab6a8f8f26366af48d33973a2f3ab95b857c5
Victoria Drake <hello@victoria.dev>
2019-08-12 13:56:38 -0400

    Update docs build script

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf docs/
-cd exampleSite
-hugo --gc --minify --themesDir ../.. -d ../docs -v -b /
+rm -rf ../docs
+cd ../exampleSite
+HUGO_THEME=hugo-theme-introduction hugo --gc --minify --themesDir ../.. -v -b / -d ../docs
