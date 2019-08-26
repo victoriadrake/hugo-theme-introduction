1882254a1c916ad7577214cf9d7d15a3120943d4
Jacques Supcik <jacques@supcik.net>
2019-08-26 13:51:47 +0200

    feat: add `timeUpdatePeriod` for periodic localtime update
    timeUpdatePeriod is a new setting (in params.home) that controls periodical update of the local time. If set to zero, the localtime is not updated. If set to a value greater than zero, the localtime is updated every `timeUpdatePeriod` milliseconds. Typical values for this parameter are either 0 (update disabled) or 1000 (update every second).

--- assets/js/initMoment.js
+++ assets/js/initMoment.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-    var time = moment().tz("{{ .Site.Params.home.timeZone }}").format("{{ .Site.Params.home.timeFormat }}");
-    $("#time").html(time);
+    var update_localtime = function(){
+        var time = moment().tz("{{ .Site.Params.home.timeZone }}").format("{{ .Site.Params.home.timeFormat }}");
+        $("#time").html(time);
+    }
+    update_localtime();
+    {{if gt .Site.Params.home.timeUpdatePeriod 0}} setInterval(update_localtime, {{ .Site.Params.home.timeUpdatePeriod }}); {{end}}
 })
